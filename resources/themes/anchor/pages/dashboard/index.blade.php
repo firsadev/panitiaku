d6841f59c034c0c1b267a6f8edeb29a597c03b28
firsadev28 <firsadev28@gmail.com>
2024-12-24 14:49:12 +0000

    oke

--- resources/themes/anchor/pages/dashboard/index.blade.php
+++ resources/themes/anchor/pages/dashboard/index.blade.php
@@ -19,9 +19,9 @@
             <x-app.dashboard-card
 				href="https://devdojo.com/wave/docs"
 				target="_blank"
-				title="Documentation"
-				description="Learn how to customize your app and make it shine!"
-				link_text="View The Docs"
+				title="Buat Event"
+				description="Mulai buat event hari ini"
+				link_text="Lengkapi"
 				image="/wave/img/docs.png"
 			/>
 			<x-app.dashboard-card
